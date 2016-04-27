e07775515b4e5120d08d1b40d4d28f5765673149
agustincarmona <agustincarmona@gmail.com>
2016-04-27 12:32:21 +0200

    unit test files was separated by object

---
+++ tests/Class_test.js
@@ -0,0 +1,18 @@
+﻿/*
+*/
+'use strict';
+
+var assert = require('assert');
+var moduleObject = require('../extensions/Object');
+var moduleClass = require('../Class');
+
+describe('Class module', function() {
+	
+	it('extend class', function() {
+		var myClass = moduleClass.Class.extends({
+			
+		});
+		assert.equal(true, typeof myClass == 'function');
+	});
+	
+});
---
+++ tests/URL_test.js
@@ -0,0 +1,17 @@
+﻿/*
+*/
+'use strict';
+
+var assert = require('assert');
+var moduleObject = require('../extensions/Object');
+var moduleURL = require('../URL');
+
+describe('URL Module', function() {
+
+  it('should retrieve path without domain and parameters', function () {
+
+    assert.equal('/home', moduleURL.URL.getPathname('http://www.domain.com/home'));
+
+  });
+
+});
